6c9895fdcaa92fabc74c2ddb888baca695b9fb23
ConorMcNamara <conor.s.mcnamara@gmail.com>
2020-02-07 17:26:18 -0800

    Moved some tests to post_hoc_tests.py and included unit test for checking requirements.txt

--- test/test_multi_group_tests.py
+++ test/test_multi_group_tests.py
@@ -3,9 +3,7 @@ import pytest
 from StatsTest.multi_group_tests import *
 from scipy.stats import levene, f_oneway, bartlett, median_test
 from numpy.random import randint
-from statsmodels.stats.multicomp import pairwise_tukeyhsd
 from statsmodels.stats.contingency_tables import cochrans_q
-from statsmodels.stats.libqsturng import psturng
 
 
 class TestMultiGroupTests(unittest.TestCase):
@@ -110,21 +108,6 @@ class TestMultiGroupTests(unittest.TestCase):
         x2, p2 = bartlett(data_1, data_2, data_3, data_4)
         assert pytest.approx(x2) == x1
 
-    # Tukey Range Test
-
-    def test_tukeyRangeTest_kLessTwo_Error(self):
-        sample_data = [1, 2, 3, 4]
-        with pytest.raises(AttributeError, match="Need at least two groups to perform Tukey Range Test"):
-            tukey_range_test(sample_data)
-
-    def test_tukeyRangeTest_pResult(self):
-        x1, x2, x3 = [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]
-        results = tukey_range_test(x1, x2, x3)
-        model = pairwise_tukeyhsd(x1+x2+x3, groups=[0]*5 + [1]*5 + [2]*5)
-        p_vals = psturng(np.abs(model.meandiffs / model.std_pairs), len(model.groupsunique), model.df_total)
-        for i in range(3):
-            assert pytest.approx(p_vals[i]) == results[i][2]
-
     # Cochran Test
 
     def test_cochranTest_kLessThree_Error(self):
@@ -225,55 +208,6 @@ class TestMultiGroupTests(unittest.TestCase):
         x1, p1 = mood_median_test(g1, g2, g3, alternative='less')
         x2, p2, med, tbl = median_test(g1, g2, g3)
         assert pytest.approx(x2) == x1
-
-    ## Dunnett Test
-
-    def test_dunnetTest_kLessTwo_Error(self):
-        control = [10, 10, 10]
-        d1 = [10, 20, 30]
-        with pytest.raises(AttributeError, match="Cannot run Dunnett Test with less than two groups"):
-            dunnett_test(control, 0.05, d1)
-
-    def test_dunnettTest_alphaWrong_Error(self):
-        control = [10, 10, 10]
-        d1 = [10, 20, 30]
-        with pytest.raises(ValueError, match="Alpha level not currently supported"):
-            dunnett_test(control, 0.2, d1, d1)
-
-    def test_dunnettTest_results(self):
-        control = [55, 47, 48]
-        p1 = [55, 64, 64]
-        p2 = [55, 49, 52]
-        p3 = [50, 44, 41]
-        np.testing.assert_array_equal(dunnett_test(control, 0.05, p1, p2, p3), [True, False, False])
-
-    def test_dunnettTest_moreResults(self):
-        control = [51, 87, 50, 48, 79, 61, 53, 54]
-        p1 = [82, 91, 92, 80, 52, 79, 73, 74]
-        p2 = [79, 84, 74, 98, 63, 83, 85, 58]
-        p3 = [85, 80, 65, 71, 67, 51, 63, 93]
-        np.testing.assert_array_equal(dunnett_test(control, 0.05, p1, p2, p3), [True, True, False])
-
-    # Duncan's New Multi-Range Test
-    def test_duncanMultiRangeTest_kLessTwo_Error(self):
-        d1 = [10, 20, 30]
-        with pytest.raises(AttributeError, match="Cannot run Duncan Multi-Range Test with less than two groups"):
-            duncan_multiple_range_test(0.05, d1)
-
-    def test_duncanMultiRangeTest_alphaWrong_Error(self):
-        d1 = [10, 20, 30]
-        with pytest.raises(ValueError, match="Alpha level not currently supported"):
-            duncan_multiple_range_test(0.2, d1, d1)
-
-    def test_duncanMultiRangeTest_results(self):
-        data_1, data_2, data_3 = [9, 14, 11], [20, 19, 23], [39, 38, 41]
-        np.testing.assert_array_equal(duncan_multiple_range_test(0.05, data_1, data_2, data_3),
-                                      [np.array([2, 0]), np.array([2, 1]), np.array([1, 0])])
-
-    def test_duncanMultiRangeTest_moreResults(self):
-        data_1, data_2, data_3, data_4, data_5 = [10, 10, 10, 10, 9], [15, 15, 15, 15, 17], [20, 20, 20, 20, 8], [22, 22, 22, 22, 20], [10, 10, 10, 10, 14]
-        np.testing.assert_array_equal(duncan_multiple_range_test(0.05, data_1, data_2, data_3, data_4, data_5),
-                                      [np.array([3, 0]), np.array([3, 4]), np.array([3, 1]), np.array([2, 0]), np.array([2, 4]), np.array([1, 0])])
 
 
 if __name__ == '__main__':
